cd74c3e5e4eb75cddf8ec4ababaa77291c11d0a2
Nicole Adams <nicoleadams.boston@gmail.com>
2019-10-18 13:36:34 -0400

    Stretch goal, reaactstrap animation

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,15 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { Route } from "react-router-dom";
-import { Card,  CardSubtitle, CardTitle } from "reactstrap";
+import { Card,  CardSubtitle, CardTitle, Fade } from "reactstrap";
 
 
 export default function CharacterCard(props) {
+
+  const  [fadeIn, setFadein] = useState(true);
+  const toggle = () => setFadein(!fadeIn);
 
 console.log(props);
 
   return (
 
     <span>
+      <Fade in={fadeIn} tag="h5" className="mt-3">
       <Card className="card" width ="40px">
         <div className="character" key = {props.id}>
         <CardTitle>{props.name}</CardTitle>
@@ -19,6 +23,7 @@ console.log(props);
         </div>
         <Route exact path = "/CharacterList/:id" render={()=> <div key = {props.id} src = {props.image} name = {props.name} status = {props.status}/>}/>
       </Card>
+      </Fade>
     </span>
   )
 }
